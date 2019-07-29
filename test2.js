9d99fb2330c340d3373046824da536dc0f55f35a
Yoga Santosa <ysantosa@synergetic.net.au>
2019-07-30 09:49:02 +1000

    added package.json

---
+++ test2.js
@@ -0,0 +1,12 @@
+const urls = [
+  "https://www.google.com",
+  "https://stackoverflow.com",
+  "https://www.reddit.com"
+];
+
+
+const asd = urls.map (url => {
+    return { a: "123", };
+});
+
+console.log(asd);
